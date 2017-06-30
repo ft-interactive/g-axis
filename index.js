3defb8d61bff1ff12a7c32dc0ccb22b351689b89
Bob Haslett <bob.haslett@ft.com>
2017-06-30 18:29:33 +0100

    Add yLinear

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as xLinear } from './src/xLinear';
 export { default as yOrdinal } from './src/yOrdinal';
+export { default as yLinear } from './src/yLinear';
---
+++ src/yLinear.js
@@ -0,0 +1,93 @@
+import * as d3 from 'd3';
+
+export default function () {
+	let scale = d3.scaleLinear()
+		.domain([0, 10000])
+		.range([120, 0]);
+	let align = 'right';
+	let labelWidth = 0;
+	let tickSize = 300;
+	let yAxisHighlight = 0;
+	let numTicks = 5;
+	let yLabel;
+
+	function axis(parent) {
+		const yAxis = getAxis(align)
+            .ticks(numTicks)
+            .scale(scale);
+
+		yLabel = parent.append('g')
+            .attr('class', 'axis yAxis')
+            .call(yAxis);
+
+		//Calculate width of widest .tick text
+        parent.selectAll('.yAxis text').each(
+            function(){
+				labelWidth = Math.max(this.getBBox().width, labelWidth);
+			});
+
+        //Use this to amend the tickSIze and re cal the vAxis
+		yLabel.call(yAxis.tickSize(tickSize - labelWidth));
+
+		const origin = xLabel.selectAll('.tick')
+			.filter(function (d) {
+				return d === 0 || d === yAxisHighlight;
+			})
+			.classed('baseline', true);
+	}
+
+	axis.scale = (d) => {
+		scale = d;
+		return axis;
+	};
+	axis.domain = (d) => {
+		scale.domain(d);
+		return axis;
+	};
+	axis.range = (d) => {
+		scale.range(d);
+		return axis;
+	};
+	axis.align = (d) => {
+		align = d;
+		return axis;
+	};
+	axis.labelWidth = (d) => {
+		if (d === undefined) return labelWidth;
+		labelWidth = d;
+		return axis;
+	};
+	axis.tickSize = (d) => {
+		if (d === undefined) return tickSize;
+		tickSize = d;
+		return axis;
+	};
+	axis.yAxisHighlight = (d) => {
+		yAxisHighlight = d;
+		return axis;
+	};
+	axis.numTicks = (d) => {
+		numTicks = d;
+		return axis;
+	};
+	axis.align = (d) => {
+		if (!d) return align;
+		align = d;
+		return axis;
+	};
+	axis.yLabel = (d) => {
+		if (d === undefined) return yLabel;
+		yLabel = d;
+		return axis;
+	};
+
+	function getAxis(alignment){
+		return{
+			'left': d3.axisLeft(),
+			'right': d3.axisRight()
+		} [alignment];
+	}
+
+	return axis;
+
+}
