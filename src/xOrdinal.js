14102d4948b1c8f523b48f579eedbd1abd1c71c6
Ændrew Rininsland <aendrew@aendrew.com>
2017-07-18 18:15:30 +0100

    Updating xOrdinal because it was missing a lot of stuff (#3)
    * Updating xOrdinal because it was missing a lot of stuff
    
    * Forgot to build

--- src/xOrdinal.js
+++ src/xOrdinal.js
@@ -1,25 +1,36 @@
 import * as d3 from 'd3';
 
-export default function () {
+export default function xAxisOrdinal() {
+    let align = 'bottom';
     let scale = d3.scaleBand()
         .domain(['Oranges', 'Lemons', 'Apples', 'Pears'])
-        .rangeRound([40, 300]);
+        .rangeRound([0, 220])
+        .paddingInner(0.1)
+        .paddingOuter(0.05);
+    let labelWidth = 0; // eslint-disable-line
     let tickSize = 0;
     let offset = 0;
+    let xLabel;
 
     function axis(parent) {
-        const xAxis = d3.axisBottom()
+        const xAxis = getAxis(align)
             .tickSize(tickSize)
             .scale(scale);
 
-        const xLabel = parent.append('g')
-            .attr('class', 'axis xAxis')
-            .call(xAxis);
+        if (scale.domain.length > 1) {
+            scale.paddingInner(0.1);
+        } else {
+            scale.paddingInner(0.2);
+        }
 
-        xLabel.attr('transform', `translate(0,${offset})`);
+        xLabel = parent.append('g')
+            .attr('class', 'axis xAxis')
+            .attr('transform', `translate(0,${offset})`)
+            .call(xAxis);
     }
 
     axis.scale = (d) => {
+        if (!d) return scale;
         scale = d;
         return axis;
     };
@@ -31,17 +42,54 @@ export default function () {
         scale.rangeRound(d);
         return axis;
     };
+    axis.bandwidth = (d) => {
+        if (!d) return scale.bandwidth();
+        scale.bandwidth(d);
+        return axis;
+    };
+
+    axis.paddingInner = (d) => {
+        if (!d) return scale.paddingInner();
+        scale.paddingInner(d);
+        return axis;
+    };
+
+    axis.paddingOuter = (d) => {
+        if (!d) return scale.paddinguter();
+        scale.paddinguter(d);
+        return axis;
+    };
+
+    axis.xLabel = (d) => {
+        if (d === undefined) return xLabel;
+        labelWidth = d;
+        return axis;
+    };
 
     axis.tickSize = (d) => {
         if (d === undefined) return tickSize;
         tickSize = d;
         return axis;
     };
+
     axis.offset = (d) => {
         if (d === undefined) return offset;
         offset = d;
         return axis;
     };
+
+    axis.align = (d) => {
+        if (!d) return align;
+        align = d;
+        return axis;
+    };
+
+    function getAxis(alignment) {
+        return {
+            top: d3.axisTop(),
+            bottom: d3.axisBottom(),
+        }[alignment];
+    }
 
     return axis;
 }
