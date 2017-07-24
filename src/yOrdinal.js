aa64a3ba2f25ac3efcd152a9ef89ddb0acd38a2b
Bob Haslett <bob.haslett@ft.com>
2017-07-24 16:44:02 +0100

    remove tick size calculation from xOrdinal

--- src/yOrdinal.js
+++ src/yOrdinal.js
@@ -40,7 +40,7 @@ export default function () {
             labelWidth = Math.max(this.getBBox().width, labelWidth);
         });
 
-        yLabel.call(yAxis.tickSize(tickSize - labelWidth));
+        //yLabel.call(yAxis.tickSize(tickSize - labelWidth));
 
         parent.selectAll('.axis.yAxis text')
             .attr('id', 'yAxisLabel');
