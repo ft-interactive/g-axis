220d25231bdbc613a3ef5703a80dce2e19f3fb1d
Bob Haslett <bob.haslett@ft.com>
2017-08-10 14:47:07 +0100

    Add logScale, invert and adios id's

--- src/xLinear.js
+++ src/xLinear.js
@@ -2,13 +2,53 @@ import * as d3 from 'd3';
 
 export default function () {
     let scale = d3.scaleLinear()
-        .domain([0, 100])
-        .range([0, 220]);
-    let tickSize = 50;
+        .domain([0, 10000])
+        .range([120, 0]);
+    let align = 'right';
+    let invert = false;
+    let labelWidth = 0;
+    let logScale = false;
     let numTicks = 5;
-    let align = 'bottom';
+    let tickSize = 300;
     let xAxisHighlight = 0;
     let xLabel;
+    let frameName;
+
+    function axis(parent) {
+        if (logScale) {
+            const newScale = d3.scaleLog()
+            .domain(scale.domain())
+            .range(scale.range());
+            scale = newScale;
+        }
+        if (invert) {
+            const newRange = scale.range().reverse();
+            scale.range(newRange);
+        }
+
+        const xAxis = getAxis(align)
+            .ticks(numTicks)
+            .tickSize(tickSize)
+            .scale(scale);
+
+        xLabel = parent.append('g')
+          .attr('class', 'axis xAxis')
+          .call(xAxis);
+
+        xLabel.selectAll('.tick')
+            .filter(d => d === 0 || d === xAxisHighlight)
+            .classed('baseline', true);
+
+        if (frameName) {
+            console.log(frameName)
+            xLabel.selectAll('.axis.xAxis text')
+            .attr('id', frameName+'xLabel');
+            xLabel.selectAll('.axis.xAxis line')
+            .attr('id', frameName+'xTick');
+        }
+
+        xLabel.selectAll('.domain').remove();
+    }
 
     function getAxis(alignment) {
         return {
@@ -17,30 +57,12 @@ export default function () {
         }[alignment];
     }
 
-    function axis(parent) {
-        const xAxis = getAxis(align)
-            .tickSize(tickSize)
-            .ticks(numTicks)
-            .scale(scale);
-
-        xLabel = parent.append('g')
-            .attr('class', 'axis xAxis')
-            .call(xAxis);
-
-        xLabel.selectAll('.tick')
-            .filter(d => d === 0 || d === xAxisHighlight)
-            .classed('baseline', true);
-
-        xLabel.selectAll('.domain').remove();
-    }
-
     axis.align = (d) => {
         if (!d) return align;
         align = d;
         return axis;
     };
     axis.scale = (d) => {
-        if (!d) return scale;
         scale = d;
         return axis;
     };
@@ -48,8 +70,23 @@ export default function () {
         scale.domain(d);
         return axis;
     };
+    axis.frameName = (d) => {
+        if (!d) return frameName;
+        frameName = d;
+        return axis;
+    };
     axis.range = (d) => {
         scale.range(d);
+        return axis;
+    };
+    axis.labelWidth = (d) => {
+        if (d === undefined) return labelWidth;
+        labelWidth = d;
+        return axis;
+    };
+    axis.logScale = (d) => {
+        if (d === undefined) return logScale;
+        logScale = d;
         return axis;
     };
     axis.tickSize = (d) => {
@@ -57,20 +94,23 @@ export default function () {
         tickSize = d;
         return axis;
     };
+    axis.xAxisHighlight = (d) => {
+        yAxisHighlight = d;
+        return axis;
+    };
+    axis.numTicks = (d) => {
+        numTicks = d;
+        return axis;
+    };
+    axis.invert = (d) => {
+        if (d === undefined) return invert;
+        invert = d;
+        return axis;
+    };
     axis.xLabel = (d) => {
         if (d === undefined) return xLabel;
         xLabel = d;
         return axis;
     };
-    axis.numTicks = (d) => {
-        if (d === undefined) return numTicks;
-        numTicks = d;
-        return axis;
-    };
-    axis.xAxisHighlight = (d) => {
-        xAxisHighlight = d;
-        return axis;
-    };
-
     return axis;
 }
