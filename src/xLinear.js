6292afe1b0dc9612e6003abe285dbfdbe4df5487
Bob Haslett <bob.haslett@ft.com>
2017-08-11 17:50:27 +0100

    test frameName

--- src/xLinear.js
+++ src/xLinear.js
@@ -46,6 +46,10 @@ export default function () {
             .filter(d => d === 0 || d === xAxisHighlight)
             .classed('baseline', true);
 
+        if (frameName) {
+            console.log('frameName');
+        }
+
         xLabel.selectAll('.domain').remove();
     }
 
