43664af0ad68ac70c340129ecf667c6d218466e1
Bob Haslett <bob.haslett@ft.com>
2017-11-09 14:33:07 +0000

    Update decimal intervals on linear axis

--- src/xLinear.js
+++ src/xLinear.js
@@ -34,7 +34,7 @@ export default function () {
         let deciFormat;
         if (span >= 0.5) { deciFormat = d3.format('.1f'); }
         if (span < 0.5) { deciFormat = d3.format('.2f'); }
-        if (span < 0.01) { deciFormat = d3.format('.3f'); }
+        if (span < 0.1) { deciFormat = d3.format('.3f'); }
         if (span < 0.001) { deciFormat = d3.format('.4f'); }
         if (span < 0.0001) { deciFormat = d3.format('.5f'); }
         if (span < 0.00001) { deciFormat = d3.format('.6f'); }
