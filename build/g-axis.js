a27f98daf7bcde931cc12747b319e2cffe413b4c
Bob Haslett <bob.haslett@ft.com>
2017-11-09 11:39:41 +0000

    Update build

--- build/g-axis.js
+++ build/g-axis.js
@@ -25,7 +25,6 @@
         function axis(parent) {
             function getAxis(alignment) {
                 if (intraday) {
-                    console.log('intraday axis'); // eslint-disable-line
                     const newDomain = scale.domain();
                     const newRange = scale.range();
                     scale = d3.scalePoint()
@@ -334,6 +333,7 @@
         let scale = d3.scaleLinear()
             .domain([0, 100])
             .range([0, 220]);
+        let divisor = 1;
         let tickSize = 50;
         let numTicks = 5;
         let align = 'bottom';
@@ -351,10 +351,24 @@
         }
 
         function axis(parent) {
+            let deciCheck = false;
+            let span = scale.domain()[1] - scale.domain()[0];
+
             if (invert) {
                 const newRange = scale.range().reverse();
+                span = scale.domain()[0] - scale.domain()[1];
                 scale.range(newRange);
             }
+
+            let deciFormat;
+            if (span < 10) { deciFormat = d3.format('.1f'); }
+            if (span < 0.1) { deciFormat = d3.format('.2f'); }
+            if (span < 0.01) { deciFormat = d3.format('.3f'); }
+            if (span < 0.001) { deciFormat = d3.format('.4f'); }
+            if (span < 0.0001) { deciFormat = d3.format('.5f'); }
+            if (span < 0.00001) { deciFormat = d3.format('.6f'); }
+            const numberFormat = d3.format(',');
+
             if (logScale) {
                 const newScale = d3.scaleLog()
                 .domain(scale.domain())
@@ -365,7 +379,19 @@
             const xAxis = getAxis(align)
                 .tickSize(tickSize)
                 .ticks(numTicks)
-                .scale(scale);
+                .scale(scale)
+                .tickFormat(formatNumber);
+
+            function formatNumber(d) {
+                const test4Decimal = Number.isInteger(d / divisor);
+                if (test4Decimal === false) { deciCheck = true; }
+                if (d / divisor === 0) { return numberFormat(d / divisor); }
+                if (logScale) { return numberFormat(d / divisor); }
+                if (deciCheck) {
+                    return deciFormat(d / divisor);
+                }
+                return numberFormat(d / divisor);
+            }
 
             xLabel = parent.append('g')
                 .attr('class', 'axis xAxis')
@@ -388,6 +414,11 @@
         axis.align = (d) => {
             if (!d) return align;
             align = d;
+            return axis;
+        };
+        axis.divisor = (d) => {
+            if (!d) return divisor;
+            divisor = d;
             return axis;
         };
         axis.frameName = (d) => {
@@ -542,6 +573,7 @@
             .domain([0, 10000])
             .range([120, 0]);
         let align = 'right';
+        let divisor = 1;
         let invert = false;
         let labelWidth = 0;
         let logScale = false;
@@ -552,6 +584,9 @@
         let frameName;
 
         function axis(parent) {
+            let deciCheck = false;
+            let span = scale.domain()[1] - scale.domain()[0];
+
             if (logScale) {
                 const newScale = d3.scaleLog()
                 .domain(scale.domain())
@@ -560,12 +595,34 @@
             }
             if (invert) {
                 const newRange = scale.range().reverse();
+                span = scale.domain()[0] - scale.domain()[1];
                 scale.range(newRange);
             }
 
+            let deciFormat;
+            if (span < 10) { deciFormat = d3.format('.1f'); }
+            if (span < 0.1) { deciFormat = d3.format('.2f'); }
+            if (span < 0.01) { deciFormat = d3.format('.3f'); }
+            if (span < 0.001) { deciFormat = d3.format('.4f'); }
+            if (span < 0.0001) { deciFormat = d3.format('.5f'); }
+            if (span < 0.00001) { deciFormat = d3.format('.6f'); }
+            const numberFormat = d3.format(',');
+
             const yAxis = getAxis(align)
                 .ticks(numTicks)
-                .scale(scale);
+                .scale(scale)
+                .tickFormat(formatNumber);
+
+            function formatNumber(d) {
+                const test4Decimal = Number.isInteger(d / divisor);
+                if (test4Decimal === false) { deciCheck = true; }
+                if (d / divisor === 0) { return numberFormat(d / divisor); }
+                if (logScale) { return numberFormat(d / divisor); }
+                if (deciCheck) {
+                    return deciFormat(d / divisor);
+                }
+                return numberFormat(d / divisor);
+            }
 
             yLabel = parent.append('g')
               .attr('class', 'axis yAxis')
@@ -611,6 +668,11 @@
         axis.align = (d) => {
             if (!d) return align;
             align = d;
+            return axis;
+        };
+        axis.divisor = (d) => {
+            if (!d) return divisor;
+            divisor = d;
             return axis;
         };
         axis.frameName = (d) => {
