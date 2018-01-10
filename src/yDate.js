6f9644f9c1fa3c1bb492687d52cead0caa64cc60
Bob Haslett <bob.haslett@ft.com>
2018-01-10 14:59:23 +0000

    Add label functionality to the yDate axis

--- src/yDate.js
+++ src/yDate.js
@@ -16,7 +16,7 @@ export default function () {
     let fullYear = false;
     let align = 'left';
     let yLabel;
-    let lable;
+    let label;
     let yLabelMinor;
     let endTicks;
     let customFormat = false;
@@ -134,6 +134,62 @@ export default function () {
             if (minorAxis) {
                 yLabelMinor.selectAll('.axis.yAxis line')
                 .attr('id', `${frameName}xTick`);
+            }
+        }
+        if (label) {
+            let defaultLabel = {
+                tag: label.tag,
+                hori: 'left',
+                vert: 'middle',
+                anchor: 'middle',
+                rotate: -90,
+            };
+            if (label.hori) {
+                defaultLabel.hori = label.hori;
+            }
+            if (label.vert) {
+                defaultLabel.vert = label.vert;
+            }
+            if (label.anchor) {
+                defaultLabel.anchor = label.anchor;
+            }
+            if (label.rotate) {
+                defaultLabel.rotate = label.rotate;
+            }
+
+            let axisLabel = parent.append('g')
+                .attr('class', 'axis xAxis');
+
+            axisLabel.append('text')
+                .attr('y', getVerticle(defaultLabel.vert))
+                .attr('x', getHorizontal(align, defaultLabel.hori))
+                .text(defaultLabel.tag)
+
+            const text = axisLabel.selectAll('text');
+            const width = (text.node().getBBox().width) / 2;
+            const height = (text.node().getBBox().height) / 2;
+            const textX = text.node().getBBox().x + width;
+            const textY = text.node().getBBox().y + height;
+            text.attr('transform', 'rotate(' + (defaultLabel.rotate) + ', ' + textX + ', ' + textY + ')')
+                .style('text-anchor', defaultLabel.anchor);
+
+            function getVerticle(vert) {
+                return {
+                    top: scale.range()[0],
+                    middle: (scale.range()[1] - scale.range()[0]) / 2,
+                    bottom: scale.range()[1],
+                }[vert];
+            }
+
+            function getHorizontal(axisAlign, horiAlign) {
+                return {
+                    leftleft: 0 - (labelWidth + (rem / .9)),
+                    leftmiddle: 0 - (labelWidth / 2),
+                    leftright: (rem),
+                    rightleft: tickSize,
+                    rightmiddle: tickSize + (rem * 1),
+                    rightright: tickSize + (rem * 2),
+                }[axisAlign + horiAlign];
             }
         }
 
