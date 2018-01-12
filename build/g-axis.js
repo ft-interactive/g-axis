814b4ba1f603dc04ffaf4a282197e4feab166205
Bob Haslett <bob.haslett@ft.com>
2018-01-12 11:48:15 +0000

    Update Xlinera build

--- build/g-axis.js
+++ build/g-axis.js
@@ -28,8 +28,8 @@
         let tickValues;
 
         function axis(parent) {
-            let plotHeight = plotDim[1];
-            let plotWidth = plotDim[0];
+            const plotWidth = plotDim[0];
+            const plotHeight = plotDim[1];
 
             function getAxis(alignment) {
                 if (intraday) {
@@ -428,6 +428,7 @@
         let xAxisHighlight = 0;
         let xLabel;
         let label;
+        let plotDim = [200, 100];
         let rem = 10;
         let frameName;
         let tickValues;
@@ -535,9 +536,9 @@
                         toptop: 0 - (rem),
                         topmiddle: 0,
                         topbottom: 0 + (rem),
-                        bottomtop: tickSize,
-                        bottommiddle: tickSize + (rem * 1),
-                        bottombottom: tickSize + (rem * 3),
+                        bottomtop: plotHeight,
+                        bottommiddle: plotHeight + (rem * 1.6),
+                        bottombottom: plotHeight + tickSize + (rem * 1.7),
                     }[axisAlign + vertAlign];
                 }
 
@@ -585,6 +586,11 @@
         axis.label = (d) => {
             if (d === undefined) return label;
             label = d;
+            return axis;
+        };
+        axis.plotDim = (d) => {
+            if (!d) return plotDim;
+            plotDim = d;
             return axis;
         };
         axis.rem = (d) => {
