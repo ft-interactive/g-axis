e0312e4136ebee1b42ce8499d927718821c8c058
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-04-25 16:28:09 +0100

    Updating snapshot and env

--- test/__env__/puppeteer_environment.js
+++ test/__env__/puppeteer_environment.js
@@ -520,7 +520,7 @@ class CustomEnvironment extends NodeEnvironment {
         try {
             this.url = await listen(this.server);
 
-            this.browser = await launch({ args: ['--no-sandbox', '--disable-setuid-sandbox', '--enable-font-antialiasing'] });
+            this.browser = await launch({ args: ['--enable-font-antialiasing'] });
             this.global.page = await this.browser.newPage();
 
             await this.global.page.goto(this.url, { waitUntil: 'networkidle0' });
